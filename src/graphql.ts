837704a09960fc5744b980fefca52b58b5e1c510
sandro <devspuig@pm.me>
2023-03-08 08:41:15 +0100

    Create _NftModule_ and its schema definition

---
+++ src/graphql.ts
@@ -0,0 +1,32 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface TransferNftInput {
+    from?: Nullable<string>;
+    to?: Nullable<string>;
+}
+
+export interface IQuery {
+    nfts(): Nullable<Nullable<Nft>[]> | Promise<Nullable<Nullable<Nft>[]>>;
+}
+
+export interface IMutation {
+    transferNft(transferNftInput?: Nullable<TransferNftInput>): Nullable<boolean> | Promise<Nullable<boolean>>;
+}
+
+export interface Nft {
+    name?: Nullable<string>;
+    blockchainLink?: Nullable<string>;
+    description?: Nullable<string>;
+    imageUrl?: Nullable<string>;
+    mintDate?: Nullable<string>;
+}
+
+type Nullable<T> = T | null;
